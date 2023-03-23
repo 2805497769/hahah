7d1f7a844b03845901e1969e727233a08b3b8b10
2805497769 <89448722+2805497769@users.noreply.github.com>
2023-03-23 16:58:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea,date, temperature, highest, lowest = get_weather()
-data = {"date":{"value":date,"color":get_random_color()},"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()},"highest": {"value":highest,"color":get_random_color()},"lowest":{"value":lowest, "color":get_random_color()}}
+wea,date, temperature= get_weather()
+data = {"date":{"value":date,"color":get_random_color()},"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()}}
 count = 0
 
 
